4aba11d1be04296d5b4f2d25edbbd2d81ea1c5c3
Yannik Hangst <yanso1@posteo.de>
2022-06-16 22:50:52 +0200

    add structure and tailwind

---
+++ src/components/ShoppingItem.js
@@ -0,0 +1,17 @@
+function ShoppingItem() {
+  return (
+    <>
+      <section>
+        <h2>Shopping Item</h2>
+        <h3>??? per piece</h3>
+        <button>+</button>
+        <button>-</button>
+        <button>RESET</button>
+        <p>Amount: </p>
+        <p>Total: </p>
+      </section>
+    </>
+  );
+}
+
+export default ShoppingItem;
