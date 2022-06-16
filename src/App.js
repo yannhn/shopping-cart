84e9bfb1952ef2c2c3796022dd70e8f5230e7482
Yannik Hangst <yanso1@posteo.de>
2022-06-16 23:17:05 +0200

    add objects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import Header from "./components/UI/Header";
 import Footer from "./components/UI/Footer";
 import ShoppingItem from "./components/ShoppingItem";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: nanoid(), title: "Bananas", price: 0.5 },
+    { id: nanoid(), title: "Apples", price: 0.6 },
+    { id: nanoid(), title: "Avocados", price: 1.9 },
+  ]);
+
   return (
     <>
       <Header></Header>
