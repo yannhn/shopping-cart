535ec7cc613ead0ebf50dc092132b8d43097dd03
Yannik Hangst <yanso1@posteo.de>
2022-06-19 13:10:48 +0200

    center heading

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <>
-      <h1 className="bg-white font-medium leading-tight text-5xl mt-0 mb-2 text-black-600">
+      <h1 className="bg-white font-medium leading-tight text-5xl mt-0 mb-2 text-black-600 text-center p-4">
         Shopping Cart
       </h1>
     </>
